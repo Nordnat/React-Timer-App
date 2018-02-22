4546e4ac744237b2b549a8b91844fbe921eb3d23
Nordnat <rkamieniarz@gmail.com>
2018-02-22 16:57:32 +0100

    changes in nav

---
+++ app/components/Timer.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Timer = React.createClass({
+    render: function() {
+        return (
+            <h1>Timer component</h1>
+        )
+    }
+});
+    
+
+module.exports = Timer;
